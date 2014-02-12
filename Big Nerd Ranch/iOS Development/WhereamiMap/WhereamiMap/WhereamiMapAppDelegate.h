52957abd27f27d9c76d2543914a1a96fc8c7715e
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-02-12 00:42:58 -0800

    Add WhereamiMap updates.

---
+++ Big Nerd Ranch/iOS Development/WhereamiMap/WhereamiMap/WhereamiMapAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WhereamiMapAppDelegate.h
+//  WhereamiMap
+//
+//  Created by Patrick Reynolds on 2/11/14.
+//  Copyright (c) 2014 Patrick Reynolds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiMapAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
